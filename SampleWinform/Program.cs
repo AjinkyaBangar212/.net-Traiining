b2d8a1f16381671ca60a98a9aa19b74edb908972
Ajinkya Bangar <ajinkyabangar212@gmail.com>
2022-07-11 15:19:12 +0530

    Commit

--- SampleWinform/Program.cs
+++ SampleWinform/Program.cs
@@ -17,6 +17,7 @@ namespace SampleWinform
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form3());
+
         }
     }
 }
